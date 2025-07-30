76dba3d4be54393d9dcc2e02330ef7f80610aa5f
mnivedcodinoverse <nived.marumamula@codinoverse.com>
2025-07-30 12:47:09 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import "./Navbar.css";
+import "./navbar.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
