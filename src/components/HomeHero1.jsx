435c395f551993aa58a396735382223b97f988ce
mnivedcodinoverse <nived.marumamula@codinoverse.com>
2025-07-30 12:54:41 +0530

    initial commit

--- src/components/HomeHero1.jsx
+++ src/components/HomeHero1.jsx
@@ -1,5 +1,5 @@
 import img1 from "../assets/Img1.jpg";
-import img2 from "../assets/img2.jpg";
+import img2 from "../assets/Image2.jpg";
 import img3 from "../assets/img3.jpg";
 import "./HomeHero1.css"; 
 
