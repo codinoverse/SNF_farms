ddb71cc858c8b0f16962e052c0829edf6faff1a2
mnivedcodinoverse <nived.marumamula@codinoverse.com>
2025-07-30 12:45:07 +0530

    initial commit

--- src/components/HomeHero1.jsx
+++ src/components/HomeHero1.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import img1 from "../assets/Img1.jpg";
-import img2 from "../assets/img2.jpg";
+import img2 from "/SNF_farms/snf/src/assets/Img2.jpg";
 import img3 from "../assets/img3.jpg";
-import "./HomeHero1.css"; // ← Make sure to create this CSS file
+import "./HomeHero1.css"; 
 
 const HomeHero1 = () => {
     return (
